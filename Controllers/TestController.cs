0131230244fecb0590f3eb887098e01d67519832
gastonalg <gastonalg@gmail.com>
2026-02-23 20:22:55 -0300

    Update TestController.cs

--- Controllers/TestController.cs
+++ Controllers/TestController.cs
@@ -1,1 +1,17 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
+namespace pos.Controllers
+{
+    [ApiController]
+    [Route("api/test")]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        [Authorize]
+        public IActionResult Get()
+        {
+            return Ok("ACCESO OK - TOKEN VALIDO");
+        }
+    }
+}
