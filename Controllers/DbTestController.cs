32580804cf386a8ee5bf1dcb7b8b2d2523c7c6e8
gastonalg <gastonalg@gmail.com>
2026-02-24 10:58:46 -0300

    Update DbTestController.cs

--- Controllers/DbTestController.cs
+++ Controllers/DbTestController.cs
@@ -1,5 +1,4 @@
 using Microsoft.AspNetCore.Mvc;
-using MySqlConnector;
 
 namespace pos.Controllers;
 
@@ -7,26 +6,9 @@ namespace pos.Controllers;
 [Route("api/dbtest")]
 public class DbTestController : ControllerBase
 {
-    private readonly IConfiguration _config;
-
-    public DbTestController(IConfiguration config)
+    [HttpGet]
+    public IActionResult Test()
     {
-        _config = config;
-    }
-
-    [HttpGet("ping")]
-    public async Task<IActionResult> Ping()
-    {
-        var cs = _config.GetConnectionString("Default");
-        if (string.IsNullOrWhiteSpace(cs))
-            return Problem("Falta ConnectionStrings:Default en la configuración.");
-
-        await using var conn = new MySqlConnection(cs);
-        await conn.OpenAsync();
-
-        await using var cmd = new MySqlCommand("SELECT 1;", conn);
-        var result = await cmd.ExecuteScalarAsync();
-
-        return Ok(new { ok = true, result });
+        return Ok("API OK");
     }
 }
